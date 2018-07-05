4c48ef8f411bb857de922ee101ea1e725ef8b6b9
Alex <alexepisode@gmail.com>
2018-07-05 19:12:01 +0300

    Fixed errors compilling.

--- code/src/game/shared/tf/tf_player_shared.h
+++ code/src/game/shared/tf/tf_player_shared.h
@@ -147,7 +147,6 @@ public:
 	int		GetMaskClass( void )				{ return m_nMaskClass; }
 	int		GetDesiredDisguiseClass( void )		{ return m_nDesiredDisguiseClass; }
 	int		GetDesiredDisguiseTeam( void )		{ return m_nDesiredDisguiseTeam; }
-	int		GetMaskClass(void)				{ return m_nMaskClass; }
 	EHANDLE GetDisguiseTarget( void ) 	
 	{
 #ifdef CLIENT_DLL
