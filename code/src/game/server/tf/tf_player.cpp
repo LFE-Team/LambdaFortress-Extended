0010220b86ede2ddfc68ada66181b2ce0226b2d9
Alex <alexepisode@gmail.com>
2018-07-21 09:58:45 +0300

    Fixed Sapper doesn't have for Spy.

--- code/src/game/server/tf/tf_player.cpp
+++ code/src/game/server/tf/tf_player.cpp
@@ -1731,15 +1731,16 @@ void CTFPlayer::ManageBuilderWeapons( TFPlayerClassData_t *pData )
 {
 	if ( pData->m_aBuildable[0] != OBJ_LAST )
 	{
-		CEconItemView *pItem = GetLoadoutItem( GetPlayerClass()->GetClassIndex(), TF_LOADOUT_SLOT_BUILDING );
 		CTFWeaponBase *pBuilder = Weapon_OwnsThisID( TF_WEAPON_BUILDER );
 
 		// Give the player a new builder weapon when they switch between engy and spy
-		if ( pBuilder && !ItemsMatch( pBuilder->GetItem(), pItem, pBuilder ) )
+		if ( pBuilder && !GetPlayerClass()->CanBuildObject( pBuilder->GetSubType() ) )
 		{
-			pBuilder->UnEquip( this );
+			Weapon_Detach( pBuilder );
+			UTIL_Remove( pBuilder );
+
 			pBuilder = NULL;
-		}
+        }
 		
 		if ( pBuilder )
 		{
@@ -1753,10 +1754,11 @@ void CTFPlayer::ManageBuilderWeapons( TFPlayerClassData_t *pData )
 		}
 		else
 		{
-			pBuilder = (CTFWeaponBase *)GiveNamedItem( "tf_weapon_builder", pData->m_aBuildable[0], pItem );
-
+            pBuilder = (CTFWeaponBase *)GiveNamedItem( "tf_weapon_builder" );
+			
 			if ( pBuilder )
 			{
+				pBuilder->SetSubType(pData->m_aBuildable[0]);
 				pBuilder->DefaultTouch( this );				
 			}
 		}
@@ -1769,14 +1771,15 @@ void CTFPlayer::ManageBuilderWeapons( TFPlayerClassData_t *pData )
 	else
 	{
 		//Not supposed to be holding a builder, nuke it from orbit
-		CTFWeaponBase *pWpn = Weapon_OwnsThisID( TF_WEAPON_BUILDER );
+		CTFWeaponBase *pWpn = Weapon_OwnsThisID(TF_WEAPON_BUILDER);
 
-		if ( pWpn )
-		{
-			pWpn->UnEquip( this );
-		}
+		if (pWpn == NULL)
+			return;
+
+		Weapon_Detach(pWpn);
+		UTIL_Remove(pWpn);
+	    }
 	}
-}
 
 //-----------------------------------------------------------------------------
 // Purpose: 
